7fdd310b3055bc37636faca29c80a5c9857635ec
marcfir <marcfir@proton.me>
2024-10-10 14:48:13 +0000

    use bpf

--- build.rs
+++ build.rs
@@ -8,6 +8,9 @@ fn main() {
     let libxdp_dir = xdptools_dir.join("lib/libxdp");
     let headers_dir = xdptools_dir.join("headers/xdp");
 
+    let libbpf_dir = xdptools_dir.join("lib/libbpf/src");
+    let bpf_headers_dir = libbpf_dir.join("include");
+
     let status = process::Command::new("make")
         .arg("libxdp")
         .current_dir(&xdptools_dir)
@@ -15,7 +18,15 @@ fn main() {
         .expect("could not execute make");
     assert!(status.success(), "make libdxp failed");
 
+    let status = process::Command::new("make")
+        .current_dir(&libbpf_dir)
+        .status()
+        .expect("could not execute make");
+    assert!(status.success(), "make libbpf failed");
+
     println!("cargo:rustc-link-search={}", libxdp_dir.display());
+    println!("cargo:rustc-link-search={}", libbpf_dir.display());
+    println!("cargo:rustc-link-lib=static=bpf");
     println!("cargo:rustc-link-lib=static=xdp");
     println!("cargo:rustc-link-lib=elf");
     println!("cargo:rustc-link-lib=z");
@@ -24,6 +35,7 @@ fn main() {
         .header("bindings.h")
         .generate_inline_functions(true)
         .clang_arg(format!("-I{}", headers_dir.display()))
+        .clang_arg(format!("-I{}", bpf_headers_dir.display()))
         .generate()
         .expect("Unable to generate bindings")
         .write_to_file(src_dir.join("src/bindings.rs"))
